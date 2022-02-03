1864bbbdc392061280e31c8118e8f30d464be0bf
Aminulloh Zaqi <aminulloh.zaqi@gmail.com>
2022-02-03 17:19:26 +0700

    submission

---
+++ src/routes.js
@@ -0,0 +1,31 @@
+const { addBookHandler, getAllBooksHandler, getBookByIdhandler, editBookByIdhandler, deleteBookByIdHandler } = require("./handler");
+
+const routes = [
+    {
+        method: 'POST',
+        path: '/books',
+        handler: addBookHandler,
+    },
+    {
+        method: 'GET',
+        path: '/books',
+        handler: getAllBooksHandler,
+    },
+    {
+        method: 'GET',
+        path: '/books/{bookId}',
+        handler: getBookByIdhandler,
+    },
+    {
+        method: 'PUT',
+        path: '/books/{bookId}',
+        handler: editBookByIdhandler,
+    },
+    {
+        method: 'DELETE',
+        path: '/books/{bookId}',
+        handler: deleteBookByIdHandler,
+    },
+]
+
+module.exports = routes;
